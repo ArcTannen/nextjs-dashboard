5ff059b5220b118b2de854e1e85ee999ce611f3e
ArcTannen <27651404+ArcTannen@users.noreply.github.com>
2026-08-14 14:19:04 -0400

    Added Customers page.
    Components included with course project, but never added during course steps.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,34 @@
+import CustomersTable from "@/app/ui/customers/table";
+import Pagination from "@/app/ui/invoices/pagination";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { TableRowSkeleton } from "@/app/ui/skeletons";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    //page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  //const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query);
+  const totalPages = Math.ceil(customers.length / 6);
+
+  return (
+    <div className="w-full">
+      <Suspense fallback={<TableRowSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
